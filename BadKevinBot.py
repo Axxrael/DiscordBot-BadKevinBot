858964fbaea093a87754b3f63f13f0d1ed789e0d
Le, Kevin Nhan <supersource02@gmail.com>
2024-02-13 22:11:13 -0500

    Looping works!

--- BadKevinBot.py
+++ BadKevinBot.py
@@ -2,10 +2,8 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from discord.ext import tasks
 import mysql.connector
-import asyncio
-import concurrent.futures
-
 
 
 # ---------- VARIABLES AND INFORMATION ----------
@@ -19,7 +17,7 @@ sqlPassword = (os.environ.get('SQLPASSWORD'))
 named_channel_id = 1205001660976603139 # You can right click on channels to copy their id's (The current Id for BadKevinBot General VC)
 
 botIntents = discord.Intents().default()
-bot = commands.Bot(command_prefix="/",intents=botIntents) # Sets the prefix for all commands moving forward
+bot_command = commands.Bot(command_prefix="/",intents=botIntents) # Sets the prefix for all commands moving forward
 
 databaseDayLeaderboard = mysql.connector.connect( # DayLeaderboard Database connection
     host="gamesnj409.bisecthosting.com",
@@ -32,7 +30,7 @@ cursorDayLeaderboard = databaseDayLeaderboard.cursor() # cursor object
 
 
 # Built-in slash commands listed here.
-@bot.slash_command(
+@bot_command.slash_command(
     name='ping',
     description="A pong for your ping."
 )
@@ -49,52 +47,34 @@ async def ping(context):  # the function name should match the @bot.slash_comman
     print(f'Sending ping-pong...')
 
 
-# ---------- 24-hour Day Tracking Loop ----------
-def query_database():
+def query_database(): # Connects the SQL Day Tracker database to the bot
     cursorDayLeaderboard.execute("SELECT `Current Record` FROM `Day Tracker`") # Grabbing the int value of Current Record from Day Tracker
     print('SQL Table Connected!!')
-    days = cursorDayLeaderboard.fetchone(); # Day counter
-    print(f'SELECT `Current Record` FROM `Day Tracker` fetch = {days}')
+    currentRecord = cursorDayLeaderboard.fetchone(); # Day counter
+    if currentRecord == None:
+        currentRecord = 0
+    print(f'SELECT `Current Record` FROM `Day Tracker` fetch = {currentRecord}')
+
     cursorDayLeaderboard.execute("SELECT `Personal best (longest streak)` FROM `Day Tracker`") # Grabbing the int value of Personal best from Day Tracker
-    topDayCount = cursorDayLeaderboard.fetchone(); # Highest Recorded Day
-    print(f'SELECT `Personal best (longest streak)` FROM `Day Tracker` = {topDayCount}')
-    return days, topDayCount
-
-@bot.loop(seconds=24) # Runs every 24 hours automatically
-async def change_channel_name(resetCheck=1): # Does the name changing for the channel (multiplier is usually nothing except to reset, which is then set to 0)
-    loop = asyncio.get_running_loop()
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        days, topDayCount = await loop.run_in_executor(executor, query_database)
-    if resetCheck == 0:
-        if days > topDayCount:
-            print(f'New Personal Best: {days}')
-            '''cursorDayLeaderboard.execute(f"UPDATE `Day Tracker` SET `Personal best (longest streak)` = {days}")
-            databaseDayLeaderboard.commit()
-        
-        cursorDayLeaderboard.execute("UPDATE `Day Tracker` SET `Current Record` = 0") # Resets Current Record to 0
-        databaseDayLeaderboard.commit()'''
-        print(f'Resetting Current record')
-    else:
-        '''cursorDayLeaderboard.execute("UPDATE `Day Tracker` SET `Current Record` = `Current Record` + 1") # New day
-        databaseDayLeaderboard.commit()'''
-        print(f'Another successful day with wack')
-
-    '''new_name = (f'[{days}] Days since Kevin said some wacko shit')
-    channel = bot.get_channel(named_channel_id) # Grabs the channel using the channelId (right click on the channel in discord) as an argument
-    print(f"Change channel: {channel} to {new_name}")
-    await channel.edit(name=new_name) # Does the editing
-    print("Channel name changed!")'''
+    personalBest = cursorDayLeaderboard.fetchone(); # Highest Recorded Day
+    if personalBest == None:
+        personalBest = 0
+    print(f'SELECT `Personal best (longest streak)` FROM `Day Tracker` = {personalBest}')
+    return currentRecord, personalBest
 
 
-@bot.slash_command(name='wackoshit', description="Change the channel name") # Takes in /wacko shit as a command
-async def change_channel(ctx): # Initiates channel change
-    await change_channel_name()
+# ---------- 24-hour Day Tracking Loop ----------
+@tasks.loop(seconds=10) # Runs every 24 hours automatically
+async def loopcheck():
+    print("looping")
 
 
-@bot.event
+@bot_command.event
 async def on_ready():
-    await bot.sync_commands() # Sync the commands to Discord
-    print(f'Bot is ready. Logged in as {bot.user.name}')
+    await bot_command.sync_commands() # Sync the commands to Discord
+    print(f'Bot is ready. Logged in as {bot_command.user.name}')
+    query_database()
+    'loopcheck.start()'
 
 
-bot.run(clientSecret)
+bot_command.run(clientSecret)
